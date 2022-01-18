daab5b681e2ee36df52ed681431a37c135f0649f
Jimmy Kane <jbk5603@psu.edu>
2022-01-18 15:34:07 -0500

    answer attempt

--- assignment2.md
+++ assignment2.md
@@ -19,3 +19,9 @@
 
 ## How is HTML rendering via JS?
     The render function which is imported from `lit`
+
+# Separation of Element Definition and Source
+    If I remember, `src/HelloWorld.js` defines what a HelloWorld element is.  It allows for it to be packaged up in a module and shipped to other componets that may extend this element.  `./hello-world.js` informs open-wc that this specific project is going to have a custom HTML element called `<hello-world>`.
+
+# What's so special about Lit?
+    Everything!  It allows for custom HTML tags to be created in JS and this allows for them to be publish on NPMjs.com.  Meaning that anyone can pull down these custom elements and use them in their projects without having to code it themselves.  Since these elements are made in HTML/CSS, there are few dependencies and should work in almost any browser.
--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -29,22 +29,25 @@ export class HelloWorld extends LitElement {
   }
 
   __decrement() {
-    if(this.counter > 0){
-      this.counter--;
+    if (this.counter > 0) {
+      this.counter -= 1;
     }
   }
 
-  /*Added: 
+  /*
+  Added: 
     - Add a button that subtracts from the counter but won't allow going below 0.
     - Disable the subtract button when hitting 0; enable it when hitting anything other than 0.
   */
   render() {
     return html`
       <h2>${this.title} Nr. ${this.counter}!</h2>
-      <h4>${this.description}<h4/>
-      <h5>&nbsp;&nbsp;&nbsp;&nbsp;${this.bonus}<h5/>
+      <h4>${this.description}</h4>
+      <h5>&nbsp;&nbsp;&nbsp;&nbsp;${this.bonus}</h5>
       <button @click=${this.__increment}>increment</button>
-      <button @click=${this.__decrement} ?disabled=${this.counter == 0}>decrement</button>
+      <button @click=${this.__decrement} ?disabled=${this.counter === 0}>
+        decrement
+      </button>
     `;
   }
 }
